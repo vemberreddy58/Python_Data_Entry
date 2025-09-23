052cd0a5d4108c060da93cfbd692537d174b94a7
vemberreddy <vemberreddy@gmail.com>
2025-09-23 12:07:30 +0530

    final commit

--- test.sql
+++ test.sql
@@ -2,3 +2,13 @@ use  Cust_Emp
 
 
 select * from dbo.Departments
+
+
+USE dataentry;
+
+delete from dbo.PersonData 
+where id in  
+(select id from  
+	(select *,row_number() over (partition by email order by id) rn  
+	from dbo.PersonData) a  
+	where rn >1)
