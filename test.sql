6eb6cfc0bf433e6ae58bb8b09da6cfd31fb26bce
vemberreddy <vemberreddy@gmail.com>
2025-09-26 16:03:32 +0530

    new lines in sql

--- test.sql
+++ test.sql
@@ -6,9 +6,29 @@ select * from dbo.Departments
 
 USE dataentry;
 
-delete from dbo.PersonData 
+ delete from dbo.PersonData 
 where id in  
 (select id from  
 	(select *,row_number() over (partition by email order by id) rn  
 	from dbo.PersonData) a  
 	where rn >1)
+
+
+select * from [dbo].[PersonData]
+
+
+begin tran
+delete from [dbo].[PersonData]
+rollback 
+commit 
+
+
+begin 
+	if exists (select 1 from [dbo].[PersonData])
+	 select distinct name  from  [dbo].[PersonData]
+	if not exists(select 1 from [dbo].[PersonData])
+	 print 'no names'
+end 
+
+
+
